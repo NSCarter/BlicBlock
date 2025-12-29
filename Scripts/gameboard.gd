9ae3c63246f34f70379a1f16f6497b87d3c21acd
Niamh Carter <niamh.carter13@gmail.com>
2025-12-29 18:05:00 +0000

    New block spawns when one reaches the ground

--- Scripts/gameboard.gd
+++ Scripts/gameboard.gd
@@ -1,27 +1,52 @@
 extends Node2D
 
-var colours = ["blue", "green", "pink", "yellow"]
-var block_length = 69
+const colours = ["blue", "green", "pink", "yellow"]
+const block_length = 69
 var falling_block: Block
 var falling_timer: Timer
+var wait_timer: Timer
+var blocks = [[], [], [], [], []]
 
 func _ready() -> void:
-	var block = create_block()
+	create_block()
+	wait_timer = Timer.new()
+	add_child(wait_timer)
+	wait_timer.timeout.connect(_on_wait_timer_timeout)
+	wait_timer.wait_time = 1.0
+	wait_timer.one_shot = true
+	
+
+func _process(_delta: float) -> void:
+	if _reached_bottom():
+		falling_timer.stop()
+		var falling_pos = int(falling_block.position.x / block_length)
+		blocks[falling_pos].append(falling_block)
+		wait_timer.start()
+	
+	
+func create_block():
+	var colour = colours.pick_random()
+	var block = Block.new(colour)
+	block.position.x = block_length * 2
+	add_child(block)
 	falling_block = block
 	falling_timer = Timer.new()
 	add_child(falling_timer)
-	falling_timer.timeout.connect(_on_timer_timeout)
+	falling_timer.timeout.connect(_on_falling_timer_timeout)
 	falling_timer.wait_time = 1.0
 	falling_timer.start()
 	
-	
-func create_block() -> Block:
-	var colour = colours.pick_random()
-	var block = Block.new(colour)
-	block.position.x = 69 * 2
-	add_child(block)
-	return block 
-	
 
-func _on_timer_timeout():
+func _on_falling_timer_timeout():
 	falling_block.position.y += block_length
+	
+func _on_wait_timer_timeout():
+	create_block()
+	
+	
+func _reached_bottom() -> bool:
+	var falling_pos = int(falling_block.position.x / block_length)
+	var stack_height = blocks[falling_pos].size() * block_length
+	var board_height = block_length * 7
+	var bottom = board_height - stack_height - block_length
+	return bottom == falling_block.position.y
